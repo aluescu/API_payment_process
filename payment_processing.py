5d2d852f62ba43c3c2a46e559660dd9504f00197
aluescu <65305265+aluescu@users.noreply.github.com>
2020-10-28 11:21:09 +0200

    Update payment_processing.py

--- payment_processing.py
+++ payment_processing.py
@@ -1,10 +1,12 @@
-## format of request
-#example format: http://127.0.0.1:5000/5295910192068929/Popescu George/2020-10-25/123/30.00
+# API_payment_process
 
-# all tests mentioned in the code have been tested
+# request format: http://address:port/bank account/name/expiring date/security code/amount
+# OK test: http://address:port/5295910192068929/Popescu George/2020-10-31/123/30.00
+# NOK test (date in past): http://address:port/5295910192068929/Popescu George/2020-10-25/123/30.00
+# NOK test (missing decimal separator): http://address:port/5295910192068929/Popescu George/2020-10-25/123/3000
 # if one of the test fails, error "400 bad request" is returned
-# if all tests are good,make request with same format to specific gateway, "200 OK" is returned
-
+# all tests mentioned in the code have been tested and working
+# if all tests are good, request is made using same format to specific gateway, "200 OK" is returned
 
 from flask import Flask
 from datetime import datetime
@@ -62,7 +64,7 @@ def ProcessPayment(card_numb, name, exp_date, sec_code, amount):
         datetime.strptime(exp_date, '%Y-%m-%d');
 
         #test date is in the past
-        if datetime.strptime(exp_date, '%Y-%m-%d') > datetime.now():
+        if datetime.strptime(exp_date, '%Y-%m-%d') < datetime.now():
             t_all_res.append("NOK");
     except ValueError:
         t_all_res.append("NOK");
